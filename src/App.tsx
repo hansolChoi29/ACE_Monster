c115dab122a4a3be35f628c953ee8034ad7f5785
hansolChoi29 <ggksthf29@gmail.com>
2025-06-11 11:19:08 +0900

    에러 테스트

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import './App.css';
+const a = 1; // <- 에러 intentionally
+
 function App() {
-console.log("이건 함수 선언식입니다"); // ❌ 함수 선언식은 금지해야 함 (func-style 위반)
-var a = 1 // ❌ var 사용 (no-var 위반)
-return <div>hi</div>
+  console.log('이건 함수 선언식입니다');
+  return <div>hi</div>;
 }
-export default App
+export default App;
