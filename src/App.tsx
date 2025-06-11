c98aea3ea976c9d1ca04d05cc701a627e7ad7592
hansolChoi29 <ggksthf29@gmail.com>
2025-06-11 11:16:45 +0900

    test:husky + lint-staged 에러 방지 테스트

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,7 @@
 import './App.css';
-const App=()=>{
-return<div>테스트121123</div>}
+function App() {
+console.log("이건 함수 선언식입니다"); // ❌ 함수 선언식은 금지해야 함 (func-style 위반)
+var a = 1 // ❌ var 사용 (no-var 위반)
+return <div>hi</div>
+}
 export default App
