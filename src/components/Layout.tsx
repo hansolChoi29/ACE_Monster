843ae86fb13c0437695e6ce5a115bbe286c98851
hansolChoi29 <ggksthf29@gmail.com>
2025-06-11 20:35:28 +0900

    style: auth ui

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,12 +2,14 @@ import { Outlet } from 'react-router-dom';
 
 export default function Layout() {
   return (
-    <div className="min-h-screen flex flex-col">
-      <main className="flex-1 w-full bg-black">
+    <div className="min-h-screen flex flex-col bg-black text-white">
+      <main className="flex-1 w-full flex items-center justify-center">
         <Outlet />
       </main>
 
-      <footer className="bg-gray-50 border-t p-4 text-center text-sm">© 2025 hansolChoi</footer>
+      <footer className="bg-black border-t border-gray-700 p-4 text-center text-sm">
+        © 2025 hansolChoi
+      </footer>
     </div>
   );
 }
--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -1,5 +1,27 @@
+import { Link } from 'react-router-dom';
+
 const Login = () => {
-  return <div>login</div>;
+  return (
+    <section className="flex flex-col items-center justify-center min-h-screen  px-4   text-[#8EA3E3]">
+      <div className="text-center ">
+        <Link to="/">
+          <h1 className="text-4xl font-bold mt-2">ACE_Monster</h1>
+        </Link>
+        <p className="mb-16 mt-2">로그인</p>
+        <div className="flex flex-col gap-2">
+          <div>
+            <p className="flex text-white font-bold">ID</p>
+            <input className="w-full border border-[#8EA3E3]   bg-transparent  text-white placeholder-gray-400  p-1 rounded-md     focus:outline-none focus:ring-0 " />
+          </div>
+          <div>
+            <p className="flex text-white font-bold">PASSWORD</p>
+            <input className="border border-[#8EA3E3]   bg-transparent  text-white placeholder-gray-400  p-1 rounded-md  w-full   focus:outline-none focus:ring-0 " />
+          </div>
+        </div>
+      </div>
+      <button className="bg-[#8EA3E3] text-white rounded px-2 py-1 mt-4">로그인</button>
+    </section>
+  );
 };
 
 export default Login;
