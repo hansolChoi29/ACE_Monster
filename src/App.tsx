5a354a5818d825bc9d4d5ee4e995667a2c20d711
hansolChoi29 <ggksthf29@gmail.com>
2025-06-11 12:09:06 +0900

    chore: rename PostCSS and Tailwind config to .cjs

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import './App.css';
 
 const App = () => {
-  return <div>App</div>;
+  return <div className="bg-red-500 text-white">Hello, Tailwind!</div>;
 };
 
 export default App;
---
+++ tailwind.config.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+  content: [
+    './src/**/*.{html,js,jsx,ts,tsx}', // React 컴포넌트와 HTML 모두 스캔
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
